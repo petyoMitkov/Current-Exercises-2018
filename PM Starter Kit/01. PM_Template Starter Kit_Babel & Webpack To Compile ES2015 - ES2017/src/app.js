ea3d598562053abf446351a322bc991dd647b052
Petyo Mitkov <petyo.mitkov@gmail.com>
2018-01-04 23:09:57 +0200

    Added PM Starter Kit Folder

---
+++ PM Starter Kit/01. PM_Template Starter Kit_Babel & Webpack To Compile ES2015 - ES2017/src/app.js
@@ -0,0 +1,5 @@
+import { person, hello } from './lib';
+
+//alert(person.name);
+console.dir(person);
+console.log(hello(person.name));
---
+++ PM Starter Kit/01. PM_Template Starter Kit_Babel & Webpack To Compile ES2015 - ES2017/src/lib.js
@@ -0,0 +1,9 @@
+export const person = {
+    name: 'John Doe',
+    location: 'Bulgaria',
+    age: 33
+};
+
+export function hello(name) {
+    return `Hello ${name}!`;
+}
